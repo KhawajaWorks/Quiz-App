d3ec3a780d81d0478e8f957e14319ebac07b5e17
KhawajaWorks <Akhmaskhawaja@gmail.com>
2021-09-30 01:54:05 +0500

    changed questions to a list of maps containing questions and ther answers

--- lib/answers.dart
+++ lib/answers.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
+  final VoidCallback selectHandler;
+  final String answerText;
+  const Answer(this.selectHandler, this.answerText);
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        style: ElevatedButton.styleFrom(primary: Colors.red),
-        onPressed: null,
-        child: Text('Answer 1'),
+        style: ElevatedButton.styleFrom(primary: Colors.blue),
+        onPressed: selectHandler,
+        child: Text(answerText),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -24,10 +24,19 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      "What's your favorite colour?",
-      "What's your favorite hobby?",
-      "What's your favorite food?"
+    const questions = [
+      {
+        "questionText": "What's your favorite colour?",
+        "answers": ["Black", "White", "Green", "Red"]
+      },
+      {
+        "questionText": "What's your favorite animal?",
+        "answers": ["Rabbit", "Snake", "Lion", "Bull"]
+      },
+      {
+        "questionText": "What's your favorite food?",
+        "answers": ["Pizza", "Pie", "Bread", "Pastry"]
+      },
     ];
 
     return MaterialApp(
@@ -37,13 +46,15 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Question(questions[_questionIndex]),
-            Answer(),
-            Answer(),
-            Answer(),
+            Question(
+              questions[_questionIndex]['questionText'].toString(),
+            ),
+            ...(questions[_questionIndex]['answers'] as List<String>)
+                .map((answer) {
+              return Answer(_answerQuestion, answer);
+            }).toList()
           ],
         ),
-        backgroundColor: Colors.lightGreenAccent,
       ),
     );
   }
